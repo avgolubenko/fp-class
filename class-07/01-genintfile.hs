66b626b5d564dddfff4f50acf815953692b2e532
avgolubenko <avgolubenko@yandex.ru>
2014-10-28 01:14:32 +0300

    1

--- class-07/01-genintfile.hs
+++ class-07/01-genintfile.hs
@@ -6,5 +6,22 @@
   3) количество чисел в строке;
   4) количество строк в файле.
 -}
+import System.Random
+import Control.Monad(when)
 
-main = undefined
+
+{-gen randNum len = do
+	gen <- getStdGen
+	return $ take len $ randomRs ('a','z') gen-}
+	
+--генерация списка случайных чисел заданной длины и заданного диапазона
+--listRandInt::Int->Int->Int->[Int]
+randInt from to =  randomRIO (from, to) 
+
+
+--listRandInt = randoms (mkStdGen 11) 
+--создание строки с заданным количеством случайных чисел, разделенных пробелом
+--strOfRandInt::count->[Int]->String
+strOfRandInt count from to = show randomRIO(from, to) ++" "
+--l1::a->a->a->IO[a]->[a]
+--l1 count from to = print ($listRandInt count from to)
