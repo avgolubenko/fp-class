12e76f982ec9e5117e5580765aee04288afbf234
avgolubenko <avgolubenko@yandex.ru>
2014-10-28 16:15:01 +0300

    1

--- class-07/01-genintfile.hs
+++ class-07/01-genintfile.hs
@@ -7,21 +7,44 @@
   4) количество строк в файле.
 -}
 import System.Random
-import Control.Monad(when)
+import Data.List
+import System.IO
+import Data.Char
+import System.Environment
+
+--randInt from to =  randomRIO (from, to) 
+listRandInt count from to r = take count $ randomRs (from,to) (mkStdGen r) :: [Int]
+
+--вывод 1-го элемента
+getSt count from to r = (show $head list) ++ " "
+	where
+		list = listRandInt count from to r
+
+--преобразование списка в строку, элементы через пробел		
+func [] = "\n"
+func (x:xs) =  (show x) ++ " " ++ func (xs)
+
+--строка содержащая случайные числа
+strOfRandInt count from to r = func list
+	where
+		list = listRandInt count from to r
+		
+		
+--append file [str] = appendFile file ("\n" ++ str)
+
+genFileRand file from to count scount   = do
+	if scount > 0
+	then do
+		rnd<-randomRIO(from,to)--случайное число - генератор для формирования спика случайных чисел
+		appendFile file (strOfRandInt count from to scount)
+		genFileRand file from to count rnd
+	else
+		return ()
 
 
-{-gen randNum len = do
-	gen <- getStdGen
-	return $ take len $ randomRs ('a','z') gen-}
-	
---генерация списка случайных чисел заданной длины и заданного диапазона
---listRandInt::Int->Int->Int->[Int]
-randInt from to =  randomRIO (from, to) 
+--параметры командной строки : имя файла, от , до, кол-во чисел в строке, кол-во строк в файле
 
+main = do
+	(filename : from : to : count : scount : args)<- getArgs
+	genFileRand filename (read from) (read to) (read count) (read scount)
 
---listRandInt = randoms (mkStdGen 11) 
---создание строки с заданным количеством случайных чисел, разделенных пробелом
---strOfRandInt::count->[Int]->String
-strOfRandInt count from to = show randomRIO(from, to) ++" "
---l1::a->a->a->IO[a]->[a]
---l1 count from to = print ($listRandInt count from to)
